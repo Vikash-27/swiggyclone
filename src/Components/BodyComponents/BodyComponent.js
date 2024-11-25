9d1ba9bd66d02ba9dae7ba50ae973c0fca8ff0f9
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-11-25 20:59:24 +0530

    Data fetched from api

--- src/Components/BodyComponents/BodyComponent.js
+++ src/Components/BodyComponents/BodyComponent.js
@@ -1,22 +1,45 @@
-import React from 'react'
-import BodyComponentCSS from '../../CSSFolder/BodyCSS/BodyComponentCSS.css'
-import RestaurantCardComponent from './RestaurantCardComponent'
+import React, { useState, useEffect } from 'react';
+import BodyComponentCSS from '../../CSSFolder/BodyCSS/BodyComponentCSS.css';
+import RestaurantCardComponent from './RestaurantCardComponent';
 
 const BodyComponent = () => {
-  return (
-    <div className = "Body-container">
-    <div className =  "search-container">  
-      <input className='search-box' type="text" placeholder="Search for restaurants"/>
-      <button className = "search-button">Search</button>
-    </div>
-    <div className='Restaurant-container'>
-      <div className='Res-list'>
-      <RestaurantCardComponent name = "Hotel alfa" loc = "NAD" rating = {5} food = " Pizza, Coke"/>
-      <RestaurantCardComponent name = "Muntaj" loc = "MVP" rating = {2} food = " Biriyani , Kebabs"/>
-    </div>
-    </div>
-    </div>
-  )
-}
+  const [restaurantList, setRestaurantList] = useState([]);
 
-export default BodyComponent
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.67740&lng=83.20360&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING&page=1&pageSize=20`
+        );
+        const data = await response.json();
+        console.log(data);
+        setRestaurantList(data?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="Body-container">
+      <div className="search-container">
+        <input
+          className="search-box"
+          type="text"
+          placeholder="Search for restaurants"
+        />
+        <button className="search-button">Search</button>
+      </div>
+      <div className="Restaurant-container">
+        <div className="Res-list">
+          {restaurantList.map((restaurant, index) => (
+            <RestaurantCardComponent key={index} restaurant={restaurant} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BodyComponent;
--- src/Components/BodyComponents/RestaurantCardComponent.js
+++ src/Components/BodyComponents/RestaurantCardComponent.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import SwiggyLogo from "../../Assets/swiggy_logo.png"
-import RestaurantCardComponentCSS from "../../CSSFolder/BodyCSS/RestaurantCardComponentCSS.css"
-const RestaurantCardComponent = (props) => {
-  const {name,loc,rating,food} = props
+import React from 'react';
+import RestaurantCardComponentCSS from "../../CSSFolder/BodyCSS/RestaurantCardComponentCSS.css";
+
+const RestaurantCardComponent = ({ restaurant }) => {
   return (
     <div className='RestaurantCard-container'>
-        <div className='RestaurantCardImage'>
-        </div>
-        <div className='RestaurantCard-Details'>
-        <h1> {name}</h1>
-        <br></br>
-        {loc}
-        <br></br>
-        {rating}**
-        <br></br>
-        {food}
-        </div>
+      <div className='RestaurantCardImage'>
+        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant.info.cloudinaryImageId}`} alt={restaurant.info.name} />
+      </div>
+      <div className='RestaurantCard-Details'>
+        <h2>{restaurant.info.name}</h2>
+        <p>Rating: {restaurant.info.avgRatingString} ({restaurant.info.totalRatingsString})</p>
+        <p>Location: {restaurant.info.locality}, {restaurant.info.areaName}</p>
+        <p>Cuisines: {restaurant.info.cuisines.join(', ')}</p>
+        <p>Cost for two: {restaurant.info.costForTwo}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RestaurantCardComponent
+export default RestaurantCardComponent;
