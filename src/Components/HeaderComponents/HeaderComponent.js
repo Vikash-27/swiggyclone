da7a847226851920dffb3577b9645b8f3722e5a8
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-12-02 00:54:50 +0530

    fixed finally

--- src/Components/HeaderComponents/HeaderComponent.js
+++ src/Components/HeaderComponents/HeaderComponent.js
@@ -6,18 +6,18 @@ import HeaderComponentCSS from '../../CSSFolder/HeaderCSS/HeaderComponentCSS.css
 const Header = () => {
   return (
     <div className='header-container'>
-        < div className = "logo-container">
-        <img  className = "swiggylogo" src= {Swiggylogo} alt = "Swiggy logo"/>
-        </div>   
-        <div className='nav-items'>
-          <ul className='itemslist'>
-            <li>Home</li>
-            <li>Abouts Us</li>
-            <li>Contact us</li>
-            <li>Cart</li>
-          </ul>
-
-        </div>
+      <div className="logo-container">
+        <img className="swiggylogo" src={Swiggylogo} alt="Swiggy logo" />
+        <span className="copyright-text">&copy; Vikash</span>
+      </div>   
+      <div className='nav-items'>
+        <ul className='itemslist'>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   )
 }
